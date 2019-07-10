bfdf92954e2d745151da924d7fc3b977b9253d86
Sam Howard <howardsl1@appstate.edu>
2019-07-10 11:31:59 -0400

    travis test

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("A Test");
-        System.out.println(1 + 2);
+        System.out.println(11);
     }
 }
