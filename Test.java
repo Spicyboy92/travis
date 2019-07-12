2d1d952ab4197c38a77cff3ac566c6085531630e
Sam Howard <howardsl1@appstate.edu>
2019-07-11 22:09:07 -0400

    New Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,6 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("A Test");
-        System.out.println(4);
+        System.out.println("Hi");
     }
 }
